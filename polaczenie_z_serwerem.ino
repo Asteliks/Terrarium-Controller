257825d03ab91daab305659ea13ff5f04517b21c
Asteliks <axelsz@wp.pl>
2020-05-02 12:58:26 +0200

    added temp and humid as float

--- polaczenie_z_serwerem.ino
+++ polaczenie_z_serwerem.ino
@@ -18,24 +18,26 @@ const char* serverNameHumi = "http://192.168.4.1/humidity";
 const char* serverNamePres = "http://192.168.4.1/pressure";
 
 // DHT Sensor
-uint8_t DHTPin = 4; 
-               
+uint8_t DHTPin = 4;
+
 // Initialize DHT sensor.
-DHT dht(DHTPin, DHTTYPE);                
+DHT dht(DHTPin, DHTTYPE);
 
 float Temperature;
 float Humidity;
+float setTemperature;
+float setHumidity;
 
 unsigned long poprzedniCzas = 0;
 const long oczekiwanie = 5000; //5000ms
- 
+
 void setup() {
   Serial.begin(115200);
   delay(100);
-  
+
   pinMode(DHTPin, INPUT);
 
-  dht.begin();              
+  dht.begin();
 
   Serial.println("Trwa laczenie z ");
   Serial.println(ssid);
@@ -45,8 +47,8 @@ void setup() {
 
   //wypisuj kropki podczas laczenia
   while (WiFi.status() != WL_CONNECTED) {
-  delay(1000);
-  Serial.print(".");
+    delay(1000);
+    Serial.print(".");
   }
   Serial.println("");
   Serial.println("o Boi jestem polaczony!");
@@ -54,51 +56,77 @@ void setup() {
 
 }
 void loop() {
-  
-unsigned long aktualnyCzas = millis();
 
-if(aktualnyCzas - poprzedniCzas >= oczekiwanie) {
-//  Sprawdz czy sie polaczyles z WiFi
-  if(WiFi.status()==WL_CONNECTED){
-//   do dopisania zbierania zmiennych ustawien
-     String odp = httpGETDATA(serverUstawienia); //przerabia info z serwera na string
-     odp = odp.substring(3,odp.length()); //pbcina pierwsze 3 znaki
-     char dupajasiu[odp.length()]; //tworzymy + przerabiamy na char
-     odp.toCharArray(dupajasiu,odp.length());
-     char* Tem = strtok(dupajasiu," :\{\"temp,"); //zwraca pierwsza zmienna
-    while (Tem != NULL){
-    Serial.println(Tem);
-    Tem = strtok(NULL," :\{\"temp,wilg");
+  unsigned long aktualnyCzas = millis();
+
+  if (aktualnyCzas - poprzedniCzas >= oczekiwanie) {
+    //  Sprawdz czy sie polaczyles z WiFi
+    if (WiFi.status() == WL_CONNECTED) {
+      //   do dopisania zbierania zmiennych ustawien
+      String odp = httpGETDATA(serverUstawienia); //przerabia info z serwera na string
+      setTemperature = getIntX(odp,1);
+      setHumidity = getIntX(odp,2);
+      Serial.print(setTemperature);
+      Serial.print(" *C, ");
+      Serial.print(setHumidity);
+      Serial.println(" %");
+//      odp = odp.substring(3, odp.length()); //pbcina pierwsze 3 znaki
+//      char dupajasiu[odp.length()]; //tworzymy + przerabiamy na char
+//      odp.toCharArray(dupajasiu, odp.length());
+//      char* Tem = strtok(dupajasiu, " :\{\"temp,"); //zwraca pierwsza zmienna
+//      while (Tem != NULL) {
+//        Serial.println(Tem);
+//        Tem = strtok(NULL, " :\{\"temp,wilg");
+//      }
+
+
     }
-
-
   }
-}
-delay(1000);
+  delay(1000);
 }
 
 String httpGETDATA(const char* linkSerwera) {
   HTTPClient http;
 
-//  Vader the time has come
+  //  Vader the time has come
   http.begin(linkSerwera);
 
-// Do it,... do it now
+  // Do it,... do it now
   int httpOdpowiedz = http.GET();
 
   String ladunek = "--";
 
   if (httpOdpowiedz > 0)  {
     Serial.print("Sir! Serwer mowi: ");
-//    Serial.print(httpOdpowiedz);
-    ladunek=http.getString();
+    //    Serial.print(httpOdpowiedz);
+    ladunek = http.getString();
   }
   else {
     Serial.print("Sir! Jest cicho, zbyt cicho: ");
     Serial.print(httpOdpowiedz);
   }
-// Vader release him at once
+  // Vader release him at once
   http.end();
 
   return ladunek;
+}
+
+float getIntX(String odp, int x) {
+  //  zdobywa int z odp
+  odp = odp.substring(3, odp.length()); //pbcina pierwsze 3 znaki
+  char wiadomosc[odp.length()]; //tworzymy + przerabiamy na char
+  odp.toCharArray(wiadomosc, odp.length());
+  char* zmienna = strtok(wiadomosc, " :\{\"temp,"); //zwraca pierwsza zmienna
+  if (x == 1) {
+//    Serial.print(zmienna);    
+//    Serial.println(atof("3.4"));
+//    Serial.println(atof(zmienna));
+    return atof(zmienna);
+  }
+  else {
+    //    while (Tem != NULL){
+    //    Serial.println(Tem);
+    zmienna = strtok(NULL, " :\{\"temp,wilg");
+    return atof(zmienna);
+  }
 }
