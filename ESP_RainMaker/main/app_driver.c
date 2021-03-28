9284a800397049b3e7ae350159babeb9d12d8cf5
Asteliks <axelsz@wp.pl>
2021-03-28 13:41:07 +0200

    added LED drivers and setup

--- ESP_RainMaker/main/app_driver.c
+++ ESP_RainMaker/main/app_driver.c
@@ -11,15 +11,19 @@
 #include <freertos/timers.h>
 
 #include <iot_button.h>
+#include "esp_log.h"
 #include <esp_rmaker_core.h>
 #include <esp_rmaker_standard_types.h>
 #include <esp_rmaker_standard_params.h>
 
 #include <app_reset.h>
-#include <ws2812_led.h>
+#include "driver/rmt.h"
+#include "led_strip.h"
 #include "app_priv.h"
 
 #include <dht.h>
+
+static const char *TAG = "app_driver";
 
 /* DHT sensor definition */
 static const dht_sensor_type_t sensor_type = DHT_TYPE_AM2301;
@@ -27,10 +31,21 @@ static const gpio_num_t dht1_gpio = 17;
 static const gpio_num_t dht2_gpio = 18;
 
 /* LED setup */
+#define RMT_TX_CHANNEL RMT_CHANNEL_0
 static uint16_t g_hue = DEFAULT_HUE;
 static uint16_t g_saturation = DEFAULT_SATURATION;
 static uint16_t g_value = DEFAULT_BRIGHTNESS;
 static bool g_power = DEFAULT_POWER;
+
+uint32_t red = 0;
+uint32_t green = 0;
+uint32_t blue = 0;
+uint16_t hue = 0;
+uint16_t start_rgb = 0;
+
+rmt_config_t config;
+led_strip_config_t strip_config;
+led_strip_t *strip;
 
 /* This is the button that is used for toggling the power */
 #define BUTTON_GPIO          CONFIG_EXAMPLE_BOARD_BUTTON_GPIO
@@ -46,6 +61,93 @@ static float g_temperature1 = DEFAULT_TEMPERATURE, g_temperature2 = DEFAULT_TEMP
 static float g_humidity1 = DEFAULT_HUMIDITY, g_humidity2 = DEFAULT_HUMIDITY;
 static TimerHandle_t sensor_timer;
 
+void led_strip_hsv2rgb(uint32_t h, uint32_t s, uint32_t v, uint32_t *r, uint32_t *g, uint32_t *b)
+{
+    h %= 360; // h -> [0,360]
+    uint32_t rgb_max = v * 2.55f;
+    uint32_t rgb_min = rgb_max * (100 - s) / 100.0f;
+
+    uint32_t i = h / 60;
+    uint32_t diff = h % 60;
+
+    // RGB adjustment amount by hue
+    uint32_t rgb_adj = (rgb_max - rgb_min) * diff / 60;
+
+    switch (i) {
+    case 0:
+        *r = rgb_max;
+        *g = rgb_min + rgb_adj;
+        *b = rgb_min;
+        break;
+    case 1:
+        *r = rgb_max - rgb_adj;
+        *g = rgb_max;
+        *b = rgb_min;
+        break;
+    case 2:
+        *r = rgb_min;
+        *g = rgb_max;
+        *b = rgb_min + rgb_adj;
+        break;
+    case 3:
+        *r = rgb_min;
+        *g = rgb_max - rgb_adj;
+        *b = rgb_max;
+        break;
+    case 4:
+        *r = rgb_min + rgb_adj;
+        *g = rgb_min;
+        *b = rgb_max;
+        break;
+    default:
+        *r = rgb_max;
+        *g = rgb_min;
+        *b = rgb_max - rgb_adj;
+        break;
+    }
+}
+
+esp_err_t app_light_init(void)
+{
+    rmt_config_t l_config = RMT_DEFAULT_CONFIG_TX(CONFIG_EXAMPLE_RMT_TX_GPIO, RMT_TX_CHANNEL);
+    // set counter clock to 40MHz
+    l_config.clk_div = 2;
+
+    ESP_ERROR_CHECK(rmt_config(&l_config));
+    ESP_ERROR_CHECK(rmt_driver_install(l_config.channel, 0, 0));
+
+    // install ws2812 driver
+    led_strip_config_t l_strip_config = LED_STRIP_DEFAULT_CONFIG(CONFIG_EXAMPLE_STRIP_LED_NUMBER, (led_strip_dev_t)l_config.channel);
+    led_strip_t *l_strip = led_strip_new_rmt_ws2812(&l_strip_config);
+    config=l_config;
+    strip_config=l_strip_config;
+    strip=l_strip;
+    if (!strip) {
+        ESP_LOGE(TAG, "install WS2812 driver failed");
+        return ESP_FAIL;
+    }
+    // Clear LED strip (turn off all LEDs)
+    ESP_ERROR_CHECK(strip->clear(strip, 100));
+    return ESP_OK;
+}
+
+esp_err_t led_set_hsv(uint32_t hue, uint32_t saturation, uint32_t brightness)
+{
+    // Show simple rainbow chasing pattern
+    ESP_LOGI(TAG, "Setting up LEDs");
+    for (int i = 0; i < CONFIG_EXAMPLE_STRIP_LED_NUMBER; i++) {
+        // Build RGB values
+        hue = i * 360 / CONFIG_EXAMPLE_STRIP_LED_NUMBER + start_rgb;
+        led_strip_hsv2rgb(hue, saturation, brightness, &red, &green, &blue);
+        // Write RGB values to strip driver
+        ESP_ERROR_CHECK(strip->set_pixel(strip, i, red, green, blue));
+    }
+    // Flush RGB values to LEDs
+    ESP_ERROR_CHECK(strip->refresh(strip, 100));
+
+    return ESP_OK;
+}
+
 esp_err_t app_light_set_led(uint32_t hue, uint32_t saturation, uint32_t brightness)
 {
     /* Whenever this function is called, light power will be ON */
@@ -55,16 +157,21 @@ esp_err_t app_light_set_led(uint32_t hue, uint32_t saturation, uint32_t brightne
                 esp_rmaker_device_get_param_by_type(light_device, ESP_RMAKER_PARAM_POWER),
                 esp_rmaker_bool(g_power));
     }
-    return ws2812_led_set_hsv(hue, saturation, brightness);
+    return led_set_hsv(hue, saturation, brightness);
+}
+
+esp_err_t led_clear(void){
+    ESP_ERROR_CHECK(strip->clear(strip, 100));
+    return ESP_OK;
 }
 
 esp_err_t app_light_set_power(bool power)
 {
     g_power = power;
     if (power) {
-        ws2812_led_set_hsv(g_hue, g_saturation, g_value);
+        led_set_hsv(g_hue, g_saturation, g_value);
     } else {
-        ws2812_led_clear();
+        led_clear();
     }
     return ESP_OK;
 }
@@ -85,19 +192,6 @@ esp_err_t app_light_set_saturation(uint16_t saturation)
     return app_light_set_led(g_hue, g_saturation, g_value);
 }
 
-esp_err_t app_light_init(void)
-{
-    esp_err_t err = ws2812_led_init();
-    if (err != ESP_OK) {
-        return err;
-    }
-    if (g_power) {
-        ws2812_led_set_hsv(g_hue, g_saturation, g_value);
-    } else {
-        ws2812_led_clear();
-    }
-    return ESP_OK;
-}
 
 static void app_sensor_update(void *priv)
 {
